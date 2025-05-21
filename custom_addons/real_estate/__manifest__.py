41b9c4e0301df6bfdb4e1411864beb55c35d8271
uniquetheo <unique.theo98@gmail.com>
2025-05-21 17:48:25 +0000

    initial set up

---
+++ custom_addons/real_estate/__manifest__.py
@@ -0,0 +1,16 @@
+{
+    "name":"real_estate",
+    "version":"1.0",
+    "author":"Theo Mercifield",
+    "category":"Real Estate",
+    "sequence":-10,
+    "summary":"Real Estate Management",
+    "description":"""A module to manage real estate properties, clients, and transactions.""",
+    "depends": ["base"],
+    "data": [
+        # 'views/property.xml',
+        # 'views/menu.xml',
+    ],
+    "installable": True,
+    "application": True
+}
